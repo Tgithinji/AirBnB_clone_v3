85fefe94bd54c89d116cea17545b38eeff322e7b
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-10-30 00:59:26 +0300

    Add ahandler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
  start your API!
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -19,6 +19,11 @@ def teardown(exception):
     close class storage
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not Found"}), 404)
 
 
 if __name__ == "__main__":
